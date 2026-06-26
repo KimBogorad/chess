698040129a3b4d3b07d485579ba10c97102e7fef
Kimbo <kimbog@post.bgu.ac.il>
2026-06-26 21:51:18 +0300

    updated class design, removed Row, Column, Cell design - going to be accomplished through holding a GamePiece[][] matrix in the Board class.

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,19 +1,9 @@
 public abstract class GamePiece {
     protected boolean color;
     protected boolean is_alive;
-    protected Cell pos;
 
-    public GamePiece(Cell cell, boolean color) {
-        this.pos = cell;
+    public GamePiece(boolean color) {
         this.color = color;
         this.is_alive = true;
-    }
-
-    public boolean IsLegalMove(Cell new_cell) {
-        return true;
-    }
-
-    public void SetPosition(Cell new_cell) {
-        if(IsLegalMove(new_cell)) this.pos = new_cell;
     }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -1,20 +1,5 @@
 public class Pawn extends GamePiece {
-
-    public Pawn(Cell cell, boolean color) {
-        super(cell, color);
-    }
-
-    @Override
-    public boolean IsLegalMove(Cell new_cell) {
-        Position current_pos = this.pos.GetPosition();
-        Position new_pos = new_cell.GetPosition();
-        int x_diff = current_pos.DistanceX(new_pos);
-        int y_diff = current_pos.DistanceY(new_pos);
-        // if Pawn ate enemy GamePiece:
-        if(new_cell.IsOccupied() && (x_diff == 1 || x_diff == -1) && (y_diff == 1)) return true;
-        // else: if made any illegal move
-        if (this.pos.IsOccupied() || (x_diff != 0) || (y_diff > 2)
-            || (y_diff == 2 && current_pos.GetY() != 2)) return false;
-        return true;
+    public Pawn(boolean color) {
+        super(color);
     }
 }
